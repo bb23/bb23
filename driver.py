d3bee734bbf5551e53fc4905011e95d44dcb8d2d
bb23 <23andbb@gmail.com>
2016-05-11 22:22:40 -0700

    rear wheels

--- driver.py
+++ driver.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 from time import sleep
+
 GPIO.setmode(GPIO.BOARD)
 
-forward1 = 18
-backward1 = 16
-pwm1 = 12
+# Right front motor
+rf_forward = 18
+rf_backward = 16
+rf_pwm = 12
 
+#Right Rear Motor
+rr_forward = 33
+rr_backward = 31
+rr_pwm = 29
 
-forward2 = 15
-backward2 = 13
-pwm2 = 11
+# Left front motor
+lf_forward = 15
+lf_backward = 13
+lf_pwm = 11
+
+#left Rear Motor
+lr_forward = 36
+lr_backward = 37
+lr_pwm = 32
+
 
 HIGH = GPIO.HIGH
 LOW = GPIO.LOW
@@ -19,54 +32,97 @@ OUT = GPIO.OUT
 
 class Driver(object):
     def __init__(self):
-        # Initialize motor1
-        GPIO.setup(forward1, OUT)
-        GPIO.setup(backward1, OUT)
-        GPIO.setup(pwm1, OUT)
+        # Initialize Right Front Motor
+        GPIO.setup(rf_forward, OUT)
+        GPIO.setup(rf_backward, OUT)
+        GPIO.setup(rf_pwm, OUT)
 
-        # Initialize motor2
-        GPIO.setup(forward2, OUT)
-        GPIO.setup(backward2, OUT)
-        GPIO.setup(pwm2, OUT)
+        # Initialize Left Front Motor
+        GPIO.setup(lf_forward, OUT)
+        GPIO.setup(lf_backward, OUT)
+        GPIO.setup(lf_pwm, OUT)
+        
+        # Initialize Right Rear Motor
+        GPIO.setup(rr_forward, OUT)
+        GPIO.setup(rr_backward, OUT)
+        GPIO.setup(rr_pwm, OUT)
+        
+        # Initialize Left Rear Motor
+        GPIO.setup(lr_forward, OUT)
+        GPIO.setup(lr_backward, OUT)
+        GPIO.setup(lr_pwm, OUT)
+
         print "Initialized Driver Object."
 
     def right_motor_high_forward(self, seconds):
         print "Right motor engaged."
-        GPIO.output(forward1, HIGH)
-        GPIO.output(backward1, LOW)
-        GPIO.output(pwm1, HIGH)
+        GPIO.output(rf_forward, HIGH)
+        GPIO.output(rf_backward, LOW)
+        
+        GPIO.output(rr_forward, HIGH)
+        GPIO.output(rr_backward, LOW)
+        
+        GPIO.output(rr_pwm, HIGH)
+        GPIO.output(rf_pwm, HIGH)
 
         sleep(seconds)
-        GPIO.output(pwm1, LOW)
+
+        GPIO.output(rf_pwm, LOW)
+        GPIO.output(rr_pwm, LOW)
+
         print "Ran for %s seconds. " % seconds
         print "Right motor disengaged."
 
     def left_motor_high_forward(self, seconds):
         print "Left motor engaged."
-        GPIO.output(forward2, HIGH)
-        GPIO.output(backward2, LOW)
-        GPIO.output(pwm2, HIGH)
+        GPIO.output(lf_forward, HIGH)
+        GPIO.output(lf_backward, LOW)
+        GPIO.output(lr_forward, HIGH)
+        GPIO.output(lr_backward, LOW)
+        
+        GPIO.output(lr_pwm, HIGH)
+        GPIO.output(lf_pwm, HIGH)
 
         sleep(seconds)
-        GPIO.output(pwm2, LOW)
+        
+        GPIO.output(lf_pwm, LOW)
+        GPIO.output(lr_pwm, LOW)
+
         print "Ran for %s seconds. " % seconds
         print "Left motor disengaged."
 
     def forward(self, seconds):
         print "Forward engaged."
-        GPIO.output(forward1, HIGH)
-        GPIO.output(forward2, HIGH)
+        # forward pins to high
+        GPIO.output(rf_forward, HIGH)
+        GPIO.output(rr_forward, HIGH)
+        
+        GPIO.output(lf_forward, HIGH)
+        GPIO.output(lr_forward, HIGH)
+        
+        # back pins to low
+        GPIO.output(rf_backward, LOW)
+        GPIO.output(rr_backward, LOW)
 
-        GPIO.output(backward1, LOW)
-        GPIO.output(backward2, LOW)
+        GPIO.output(lf_backward, LOW)
+        GPIO.output(lr_backward, LOW)
 
-        GPIO.output(pwm1, HIGH)
-        GPIO.output(pwm2, HIGH)
+        # engage pwmm
+        GPIO.output(rf_pwm, HIGH)
+        GPIO.output(lf_pwm, HIGH)
+
+        GPIO.output(rr_pwm, HIGH)
+        GPIO.output(lr_pwm, HIGH)
 
         sleep(seconds)
+
         print "Ran for %s seconds" % seconds
-        GPIO.output(pwm1, LOW)
-        GPIO.output(pwm2, LOW)
+        GPIO.output(rf_pwm, LOW)
+        GPIO.output(lf_pwm, LOW)
+        
+        GPIO.output(rr_pwm, LOW)
+        GPIO.output(lr_pwm, LOW)
+        
         print "Forward disengaged."
 
     def cleanup(self):
