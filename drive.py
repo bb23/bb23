c80fd290b7cbe488ce2fa3c7b46f6150bd823f59
Jesse Engelberg <jengelberg@23andme.com>
2016-07-20 21:15:39 -0700

    Made drive.py read file_status.txt and cleaned up code.

--- drive.py
+++ drive.py
@@ -19,13 +19,14 @@ BUFFER_SIZE = 30
 
 
 def send_command(command):
+    logging.info("in send_command %s" % command)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
     s.send(command + "\n")
     data = s.recv(BUFFER_SIZE)
     s.close()
 
-    print "received data:", data
+    logging.info("received data: %s" % data)
 
 """
 send_command("forward")
@@ -39,9 +40,7 @@ send_command("stop")
 
 
 def main():
-    logging.basicConfig(filename='/home/pi/bb23/example.log', level=logging.DEBUG)
-    # start_timestamp = datetime.datetime.now().strftime("%Y%m%d_%H-%M-%S.%f")
-    # image_path = "/home/pi/bb23/images/%s_%s.jpg"
+    logging.basicConfig(filename='example.log', level=logging.DEBUG)
     try:
         cam = BbCamera()
         sleep(2)
@@ -53,68 +52,28 @@ def main():
         loopery = True
         while loopery:
 
-            # # CAPTURE IMAGE
-            # with picamera.array.PiRGBArray(cam) as stream:
-            #     cam.capture(stream, 'bgr')
-            #     image = stream.array
+            with open("file_status.txt", 'r') as f:
+                status = f.read().rstrip()
 
-            # # GRAB IMAGE ATTRIBUTES
-            # height, width, channels = image.shape
-            # # assign left right center region
-            # center_x_low = width / 2 - 100
-            # center_x_high = width / 2 + 100
-
-            # # BUILD COLOR MASK WITH CONSTANTS SET FOR ~ORANGE
-            # color_mask = cv2.inRange(image, LOWER, UPPER)
-
-            # # find contours in the masked image and keep the largest one
-            # (_, cnts, _) = cv2.findContours(color_mask.copy(),
-            #                                 cv2.RETR_EXTERNAL,
-            #                                 cv2.CHAIN_APPROX_SIMPLE)
-
-            if len(cnts) == 0:
+            if status == "no_ball":
                 # Jitter'd random walk.
                 print "==================================="
                 print "     Random walk mode enabled      "
                 print "==================================="
                 random_method = random.choice(methods)
                 send_command(random_method)
-                sleep(TICKLE)
-                continue
-
-            # c = max(cnts, key=cv2.contourArea)
-
-            # # approximate the contour
-            # peri = cv2.arcLength(c, True)
-            # approx = cv2.approxPolyDP(c, 0.05 * peri, True)
-
-            # M = cv2.moments(approx)
-            # try:
-            #     c_x = int(M['m10']/M['m00'])
-            #     # c_y = int(M['m01']/M['m00'])
-            # except ZeroDivisionError:
-            #     logging.info("dividing by zero")
-            #     continue
-
-
-            if c_x < center_x_low:
+            elif status == "turn_left":
                 send_command("turn_left")
-                sleep(TICKLE/2)
-                # drive_controller.right_motor_high_forward(TICKLE/2)
-            elif c_x > center_x_high:
+            elif status == "turn_right":
                 send_command("turn_right")
-                sleep(TICKLE/2)
-                # drive_controller.left_motor_high_forward(TICKLE/2)
             else:
                 send_command("forward")
-                sleep(TICKLE/2)
-                # drive_controller.forward(TICKLE/2)
 
     except Exception as e:
         error_timestamp = datetime.datetime.now().strftime("%Y%m%d_%H:%M:%S.%f")
         import traceback
         logging.debug(error_timestamp + ": " + str(e))
-        logging.debug(traceback.print_exc(file=open('/home/pi/bb23/traceback.log','a')))
+        traceback.print_exc(file=open('traceback.log','a'))
 
 if __name__ == "__main__":
     main()
